fa7962fe4a9064a7a30acb619d98ceb8902231c8
Tybi <developer@dwastudio.fr>
2017-10-09 12:05:19 +0200

    :sparkles: add constants

---
+++ src/constants/colors.js
@@ -0,0 +1,32 @@
+// COMMON
+export const PRIMARY = '#01D88D';
+export const PRIMARY_DARK = '#01BA6C';
+export const WHITE = '#FFFFFF';
+export const PRIMARY_TEXT = '#000000';
+export const PLACE_HOLDER = '#999696';
+export const DELETE_COLOR = '#F66464';
+export const SELECTABLE_COLORS = [
+  '#F66464',
+  '#F664E9',
+  '#9864F6',
+  '#64A1F6',
+  '#647CF6',
+  '#01D88D',
+  '#F6D464',
+  '#F6A264',
+  '#77909D',
+];
+export const levelLow = '#ff605e';
+export const levelMedium = '#ffc45e';
+export const levelHigh = '#01BA6C';
+// ANDROID
+export const SEARCH_PLACEHOLDER_COLOR = '#D5D5D5';
+export const ANDROID_SEARCH_PLACEHOLDER_COLOR = '#727272';
+export const ANDROID_SETTING_IC_COLOR = '#757575';
+export const ANDROID_SEPARATOR = '#E5E5E5';
+// IOS
+export const IC_SEARCH_COLOR = '#BEBEBE';
+export const IOS_SEPARATOR = '#C8C7CC';
+export const IOS_SLIDER_LABEL = '#6D6D72';
+export const IOS_BACKGROUND = '#EFEFF4';
+export const IOS_TABICON = '#A4AAB3';
---
+++ src/constants/dimensions.js
@@ -0,0 +1,20 @@
+// ANDROID
+export const ANDROID_NAV_HEIGHT = 56;
+export const ANDROID_SEARCH_HEIGHT = 48;
+export const ANDROID_SEARCH_RADIUS = 2;
+export const ANDROID_MARGIN = 16;
+export const ACTION_BUTTON = 56;
+export const HALF_ACTION_BUTTON = ACTION_BUTTON / 2;
+export const ANDROID_BUTTON_HEIGHT = 36;
+export const ANDROID_ROW_HEIGHT = 56;
+export const ANDROID_ROW_FONTSIZE = 16;
+// iOS
+export const IOS_NAV_BAR_HEIGHT = 44;
+export const IOS_STATUS_BAR_HEIGHT = 20;
+export const IOS_FULL_HEIGHT = IOS_NAV_BAR_HEIGHT + IOS_STATUS_BAR_HEIGHT;
+export const IOS_SEARCH_HEIGHT = 28;
+export const IOS_SEARCH_RADIUS = 2;
+export const IOS_MARGIN = 8;
+export const IOS_ROW_HEIGHT = 44;
+export const IOS_ROW_FONTSIZE = 17;
+export const IOS_ICON_CTNR = 29;
