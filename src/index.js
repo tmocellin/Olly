6f507d43dd23e732a978df27acc076fa8cce27c4
Tybi <developer@dwastudio.fr>
2017-10-21 14:54:32 +0200

    :sparkles: StackNavigator

--- src/index.js
+++ src/index.js
@@ -4,6 +4,53 @@
  * @flow
  */
 
+import { Platform } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 import DrawerNavigator from './screens/DrawerNavigator';
+import TabNavigator from './screens/TabNavigator';
+import strings from './locales/strings';
+import UnlockScreen from './screens/Unlock';
+import SetupScreen from './screens/Setup';
+import ReadOnlyScreen from './screens/ReadOnly';
+import EditScreen from './screens/Edit';
 
-export default DrawerNavigator;
+const NestedNav = Platform.OS === 'Android' ? DrawerNavigator : TabNavigator;
+
+const App = StackNavigator({
+  Setup: {
+    screen: SetupScreen,
+    navigationOptions: () => ({
+      title: strings.setup,
+      headerStyle: { backgroundColor: '#01D88D' },
+      headerTintColor: 'white',
+    }),
+  },
+  Unlock: {
+    screen: UnlockScreen,
+    navigationOptions: () => ({
+      header: null,
+    }),
+  },
+  App: {
+    screen: NestedNav,
+    navigationOptions: () => ({
+      header: null,
+    }),
+  },
+  ReadOnly: {
+    screen: ReadOnlyScreen,
+    navigationOptions: ({ navigation }: Object) => ({
+      title: navigation.state.params.siteName,
+      headerStyle: { backgroundColor: '#01D88D' },
+      headerTintColor: 'white',
+    }),
+  },
+  Edit: {
+    screen: EditScreen,
+    navigationOptions: ({ navigation }: Object) => ({
+      header: null,
+    }),
+  },
+});
+
+export default App;
