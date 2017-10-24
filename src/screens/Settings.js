26432cb04deb4e90f9f5ffb4bbd2e05227df253b
Tybi <developer@dwastudio.fr>
2017-10-24 15:15:02 +0200

    :hammer: connect settings screen to redux

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -4,6 +4,10 @@
 
 import React, { Component } from 'react';
 import { View, Alert } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import type { ReduxState } from '../reducers/types';
+import * as SettingsActions from '../actions/settings';
 import { PlateformStyleSheet } from '../common/PlatformHelper';
 import SettingRow from '../components/SettingRow';
 import SliderRow from '../components/SliderRow';
@@ -12,29 +16,14 @@ import strings from '../locales/strings';
 import { IOS_BACKGROUND, WHITE, DELETE_COLOR } from '../constants/colors';
 import NavBar from '../components/NavBar';
 
-type State = {
+type Props = {
   passwordLength: number,
   autoGeneration: boolean,
+  navigation: Object,
+  actions: Object,
 };
 
-class SettingsScreen extends Component<void, void, State> {
-  state = {
-    passwordLength: 18,
-    autoGeneration: true,
-  };
-
-  setPasswordLength(length: number) {
-    this.setState({
-      passwordLength: length,
-    });
-  }
-
-  setAutoGeneration(value: boolean) {
-    this.setState({
-      autoGeneration: value,
-    });
-  }
-
+class SettingsScreen extends Component<void, Props, void> {
   deleteAllPasswords() {
     Alert.alert(strings.clear, strings.clearConfirmation, [
       { text: strings.cancel, style: 'cancel' },
@@ -54,14 +43,14 @@ class SettingsScreen extends Component<void, void, State> {
         />
         <SliderRow
           label={strings.passwordLength}
-          onValueChange={value => this.setPasswordLength(value)}
-          selectedValue={this.state.passwordLength}
+          onValueChange={value => this.props.actions.SetPasswordLength(value)}
+          selectedValue={this.props.passwordLength}
         />
         <CheckBoxRow
           label={strings.passwordAuto}
           iconName="star"
-          isChecked={this.state.autoGeneration}
-          switchValueChange={value => this.setAutoGeneration(value)}
+          isChecked={this.props.autoGeneration}
+          switchValueChange={value => this.props.actions.SetAutoGeneration(value)}
         />
         <View style={styles.itemContainer}>
           <SettingRow
@@ -77,7 +66,15 @@ class SettingsScreen extends Component<void, void, State> {
   }
 }
 
-export default SettingsScreen;
+function mapStateToProps(state: ReduxState) {
+  return {
+    passwordLength: state.settings.passwordLength,
+    autoGeneration: state.settings.autoGeneration,
+  };
+}
+export default connect(mapStateToProps, dispatch => ({
+  actions: bindActionCreators(SettingsActions, dispatch),
+}))(SettingsScreen);
 
 const styles = PlateformStyleSheet({
   container: {
