98fbd927bb143253db912822fd78139a371410f9
Tybi <developer@dwastudio.fr>
2017-11-29 15:12:31 +0100

    :hammer: fix reducer issues

--- src/reducers/types.js
+++ src/reducers/types.js
@@ -30,6 +30,9 @@ export type CryptedDataState = {
 export type SynchronizationState = {
   +userLoggedToDropbox: boolean,
   +accessToken: string,
+  +pendingAction: boolean,
+  +message: string,
+  +success: boolean,
 };
 
 export type ReduxState = {
