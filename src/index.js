ee52bde860642b241dbdb0425397e13f7877b095
Tybi <developer@dwastudio.fr>
2017-12-02 10:25:54 +0100

    :hammer: fix statusbar color

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { View, StyleSheet, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import configureStore from './store/configureStore';
 import Loader from './components/Loader';
 import StackNavigator from './screens/StackNavigator';
+import { PRIMARY_DARK } from './constants/colors';
 
 const { persistor, store } = configureStore();
 
 const App = () => (
-  <PersistGate persistor={persistor} loading={<Loader />}>
-    <Provider store={store}>
-      <StackNavigator />
-    </Provider>
-  </PersistGate>
+  <View style={styles.ctnr}>
+    <StatusBar backgroundColor={PRIMARY_DARK} />
+    <PersistGate persistor={persistor} loading={<Loader />}>
+      <Provider store={store}>
+        <StackNavigator />
+      </Provider>
+    </PersistGate>
+  </View>
 );
 
 export default App;
+
+const styles = StyleSheet.create({
+  ctnr: {
+    flex: 1,
+  },
+});
